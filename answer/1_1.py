56d5595208e0fecf56c489db0df003d38d75ea8a
Max Base <MaxBaseCode@Gmail.Com>
2019-07-13 21:40:40 +0430

    Create 1_1.py

---
+++ answer/1_1.py
@@ -0,0 +1,20 @@
+###
+ #
+ # @Name : GoogleInterview/1_1.py
+ # @Version : 1.0
+ # @Programmer : Max
+ # @Date : 2019-07-13
+ # @Released under : https://github.com/BaseMax/GoogleInterview/blob/master/LICENSE
+ # @Repository : https://github.com/BaseMax/GoogleInterview
+ #
+ ###
+def check(a, b):
+	n=len(a)
+	m=len(b)
+	for i in range(1,1000):
+		if b in (a * i):
+			return i
+	return -1
+print( check("abcd", "cdabcdab") ) # 3
+print( check("abcd", "cdabcdabx") ) # -1
+
